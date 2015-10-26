9696fd25c7437624d0f25e65b5d652c28db8b073
mds2 <michael.schuresko+github@gmail.com>
2015-10-26 16:19:51 -0700

    Initial commit

---
+++ dot_emacs.el
@@ -0,0 +1,10 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Replacement for my traditional `.emacs` file which can be more easily
+;; packaged in a git repo and shipped around
+;;
+;; It is intended that, should one use this, one's .emacs file should consist
+;; solely of
+;;
+;; (add-to-list 'load-path "~/.emacs.d/lisp")
+;; (load "~/.emacs.d/lisp/dot_emacs.el")
+
